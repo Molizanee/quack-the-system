b5e464e1675f76ed8d6a6e6b1640e1e880a33dfd
David Molizane <david.molizane@icloud.com>
2026-03-17 09:24:38 -0300

    feat: add project pygame boilerplate

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
